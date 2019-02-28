ef2dd05874147c0854723eef58776e2597d86eb1
曾祥达 <zengxiangda@zhongan.io>
2019-02-28 14:05:52 +0800

    init

---
+++ cropper/index.js
@@ -0,0 +1,205 @@
+// src/pages/cutImage/index.js
+const regeneratorRuntime = require('../tool/regenerator-runtime')
+import img from '../tool/image'
+const screenW = wx.getSystemInfoSync().windowWidth
+const screenH = wx.getSystemInfoSync().windowHeight
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // test for canvas & image
+    //剪裁框的大小
+    widthRpx: 360 * 2,
+    heightRpx: 160 * 2, 
+    src: null,// 初始图片路径
+    width: null,// 图片宽度，剪裁框可移动的宽度范围
+    height: null,// 图片高度，剪裁框可移动的高度范围
+    coverWidth: null,//剪裁框 宽度px
+    coverHeight: null,
+    direction: 'vertical',// 图片方向
+    w: screenW,//屏幕宽度
+    h: screenH, //屏幕高度
+    imgInfo: null, //图片信息
+    startX: null, // 移动的开始位置
+    startY: null, 
+    endX: null, // 移动的结束位置
+    endY: null,
+    cutX: null, // 剪裁的开始位置
+    cutY: null,
+    ctx: null,
+    // 初始位置
+    left: null,
+    top: null,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 一般从上级页面传图
+    this.setData({
+      src: 'https://horizon-mall-test.oss-cn-hzfinance.aliyuncs.com/lottery-mini-test/155126193241566843.jpeg',
+      ctx: wx.createCanvasContext('myCanvas'),
+    })
+  },  
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.initCover(() => {
+      this.init()
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  // 初始化 移动即图片展示大小
+  init: async function () {
+    
+    const imgInfo = await img.getInfo(this.data.src)
+
+    // 框宽高
+    const isNormal = imgInfo.height > imgInfo.width
+    const radio = imgInfo.width / imgInfo.height
+    
+    const width = isNormal ? this.data.coverWidth  : this.data.w 
+    const height = !isNormal ? this.data.w / radio  : this.data.coverWidth / radio 
+    console.log((width - this.data.coverWidth) / 2, (height - this.data.coverHeight) / 2 )
+    this.setData({
+      width,
+      height,
+      direction: isNormal ? 'vertical' : 'horizontal',
+      imgInfo,
+      
+    })
+
+  },
+  move(e) {
+    // there we get x, y
+  },
+  moveEnd(e) {
+    console.log('end', e)
+    this.setData({
+      endX: e.changedTouches[0].pageX,
+      endY: e.changedTouches[0].pageY
+    })
+  },
+  moveStart(e) {
+    console.log('start', e)
+    this.setData({
+      startX: e.changedTouches[0].pageX,
+      startY: e.changedTouches[0].pageY
+    })
+  },
+  /**
+   * 一般为图片上传，这里为了方便展示选择图片预览
+   */
+  uploadImage: function () {
+    
+    // 拿到移动距离
+    const moveX = this.data.endX - this.data.startX;
+    const moveY = this.data.endY - this.data.startY;
+    /**
+     * 通过moveX， moveY和cutX，cutY去定位canvas的画图位置
+     */
+    
+    this.initCover(  () => {
+      wx.createSelectorQuery().select('.img').boundingClientRect( async ({ height, width, left, top }) => {
+        console.log(this.data.left - left, this.data.top - top, left, top, 'init2')
+       
+        const file = await img.draw({
+          imgInfo: this.data.imgInfo,
+          startX: this.data.left - left,
+          startY: this.data.top - top ,
+          width: this.data.width,
+          height: this.data.height,
+          ctx: this.data.ctx,
+          canvasId: 'myCanvas',
+          coverWidth: this.data.coverWidth,
+          coverHeight: this.data.coverHeight,
+          self: this,
+          width: this.data.width,
+          height: this.data.height
+        })
+        // const data = await img.upload(file.data)
+        // 改成预览
+        img.preview(file.data)
+      }).exec(0)
+      
+    })
+    
+  },
+  //更换图片
+  chooseImage: async function () {
+    const {src} = await img.choose();
+    console.log(src)
+    this.setData({
+      src: src,
+    },() => {
+      this.initCover(() => {
+        this.init()
+      })
+      // 
+    })
+    
+  },
+  // 初始获取剪裁框的初始数据，宽高位置等
+  initCover(cb) {
+    wx.createSelectorQuery().select('.cut-cover').boundingClientRect(({height, width,left, top}) => {
+      console.log(width, height, left, top,'init')
+      this.setData({
+        coverWidth: width,
+        coverHeight: height,
+        left,
+        top,
+        cutX: (this.data.width - width) / 2,
+        cutY: (this.data.height - height) / 2
+      }, () => {
+        cb && cb()
+      })
+    }).exec()
+  }
+})
---
+++ tool/image.js
@@ -0,0 +1,176 @@
+const regeneratorRuntime = require('./regenerator-runtime')// async await
+let img = {};
+let uploadServer//上传图片地址
+// 选择图片
+img.choose = () => {
+  return new Promise((resolve, reject) => {
+    wx.chooseImage({
+      count: 1,
+      // 选取原图
+      sizeType: ['original'],
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+        // console.log('path', res.tempFilePaths[0], res.tempFiles)
+        const limit = 5 * 1024 * 1024
+        // 限制图片大小
+        if (res.tempFiles[0].size > limit) {
+          wx.showToast({
+            icon: 'none',
+            title: '图片大小不能超过5M',
+          })
+          reject('图片过大')
+        }else {
+          resolve(
+            {
+              src: res.tempFilePaths[0],
+              size: res.tempFiles[0].size
+            })
+        }
+        
+      },
+      fail: (e) => {
+        reject(e)
+      }
+    })
+  })
+
+};
+
+
+// 读取图片信息
+img.getInfo = (src) => {
+
+  return new Promise( (resolve, reject) => {
+    wx.getImageInfo({
+      src,
+      success: (res) => {
+        // todo 横屏照片处理
+        resolve(res)
+      },
+      fail: (e) => {
+        reject(e)
+      }
+    })
+  })
+}
+
+// 画图生成剪裁图片
+
+img.draw = (options) => {
+  console.log('draw...')
+  return new Promise( (resolve, reject) => {
+ 
+    const { imgInfo,width,height, coverWidth, coverHeight, startX, startY, canvasId, self} = options
+    
+    let ctx = wx.createCanvasContext(canvasId, self)
+    const drawConfig = {
+      sx: 0,
+      sy: 0,
+      sWidth: width ,
+      sHeight: height
+    }
+    console.log(options,  drawConfig)
+    ctx.clearRect(0,0,coverWidth, coverHeight)
+    
+
+    ctx.drawImage(imgInfo.path, 0,0,width * 2 ,height *2 );
+    ctx.draw(true, setTimeout(() =>  {
+      // 生成临时路径
+      let opt = {
+        x: startX * 2 ,
+        y: startY * 2,
+        width: coverWidth * 2 ,
+        height: coverHeight * 2 ,
+        destWidth: coverWidth * 2 ,
+        destHeight: coverHeight * 2
+      }
+      console.log(opt, startX,)
+      wx.canvasToTempFilePath({
+        ...opt,
+        quality: 0.8,
+        canvasId: canvasId,
+        success(res) {
+          console.log('临时路径', res)
+          resolve({
+            data: res.tempFilePath,
+            msg: '成功'
+          })
+
+        },
+        fail() {
+          // console.log(3332)
+          resolve({
+            data: null,
+            msg: '导出canvas失败'
+          })
+        }
+      },self)
+    }, 200))
+    
+  })
+} 
+
+// rewrite upload api
+function uploadFile(filePath) {
+  return new Promise((resolve, reject) => {
+    wx.uploadFile({
+      url: uploadServer,
+      name: 'file',
+      filePath,
+      success(res) {
+        resolve({
+          res,
+          msg: '上传成功'
+        })
+      },
+      fail(e) {
+        resolve({
+          res: null,
+          msg: '上传失败'
+        })
+      }
+    })
+  })
+}
+
+// 图片上传
+img.upload = async (src, size=0) => {
+  wx.showLoading({
+    title: '上传中...',
+    mask: true
+  })
+  let filePath = await img.compress(src, size)
+  const fileRes = await uploadFile(filePath)
+  wx.hideLoading()
+  let data = JSON.parse(fileRes.res.data)
+  return data
+}
+
+// 生成预览图片 
+img.preview = (src) => {
+  wx.previewImage({
+    urls: [src]
+  })
+}
+
+img.compress = (src, size) => {
+  // 如果图片超过3M压缩多一点
+  const quality = size > 3 * 1024 * 1024 ? 50 : 80
+  return new Promise((resolve, reject) => {
+    wx.compressImage({
+      src, // 图片路径
+      quality, // 压缩质量,
+      success: (res) => {
+        // console.log('compress success')
+        resolve(res.tempFilePath)
+      },
+      fail: (e) => {
+        // console.log('compress failed')
+        resolve(src)
+      }
+
+    })
+  })
+}
+
+export default img;
